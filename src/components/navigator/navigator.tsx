9c9d250e3fa25395938d9c716ef0d995ee9f12e2
Chiky Chan <chikychan@hotmail.com>
2025-01-07 16:36:01 +0800

    feat: Enable navigation via navigator

--- src/components/navigator/navigator.tsx
+++ src/components/navigator/navigator.tsx
@@ -17,6 +17,7 @@ import {
   NavSubItem,
   NavSubItemGroup,
 } from '@fluentui/react-nav-preview'
+import { useNavigate } from 'react-router-dom'
 import {
   isNavigationGroup,
   isNavigationItem,
@@ -35,46 +36,80 @@ export type NavigatorProps = {
 }
 
 export const Navigator = (props: NavigatorProps) => {
+  const navigate = useNavigate()
   const Dismiss = bundleIcon(Dismiss20Filled, Dismiss20Regular)
 
+  const handleNavItemClick = useCallback(
+    (it: NavigationItem) => {
+      navigate(it.path)
+      props.onHambugerClick()
+    },
+    [props, navigate],
+  )
+
   const renderNavigationItem = useCallback(
-    (it: NavigationItem) => (
+    (it: NavigationItem, sectionName?: string) => (
       <NavItem
         href={it.isLink ? it.path : undefined}
         icon={it.icon ? <it.icon /> : <Dismiss />}
         key={`nav-item-${it.title}`}
-        value={it.title}
+        value={sectionName ? `${sectionName}-${it.title}` : it.title}
+        onClick={() => handleNavItemClick(it)}
       >
         {it.title}
       </NavItem>
     ),
-    [Dismiss],
+    [handleNavItemClick, Dismiss],
   )
 
   const renderNavigationChildItem = useCallback(
-    (it: NavigationItem) => (
+    (it: NavigationItem, categoryName: string, sectionName?: string) => (
       <NavSubItem
         href={it.isLink ? it.path : undefined}
-        key={`nav-sub-item-${it.title}`}
-        value={it.title}
+        key={
+          sectionName
+            ? `nav-section-${sectionName}-category-${categoryName}-sub-item-${it.title}`
+            : `nav-category-${categoryName}-sub-item-${it.title}`
+        }
+        value={
+          sectionName
+            ? `${sectionName}-${categoryName}-${it.title}`
+            : `${categoryName}-${it.title}`
+        }
+        onClick={() => handleNavItemClick(it)}
       >
         {it.title}
       </NavSubItem>
     ),
-    [],
+    [handleNavItemClick],
   )
 
   const renderNavigationItemWithChildren = useCallback(
-    (it: NavigationItemWithChildren) => (
-      <NavCategory key={`nav-category-${it.title}`} value={it.title}>
+    (it: NavigationItemWithChildren, sectionName?: string) => (
+      <NavCategory
+        key={
+          sectionName
+            ? `nav-section-${sectionName}-category-${it.title}`
+            : `nav-category-${it.title}`
+        }
+        value={it.title}
+      >
         <NavCategoryItem
           key={`nav-category-item-${it.title}`}
           icon={it.icon ? <it.icon /> : <Dismiss />}
         >
           {it.title}
         </NavCategoryItem>
-        <NavSubItemGroup key={`nav-sub-item-group-${it.title}`}>
-          {it.children.map((child) => renderNavigationChildItem(child))}
+        <NavSubItemGroup
+          key={
+            sectionName
+              ? `nav-section-${sectionName}-sub-item-group-${it.title}`
+              : `nav-sub-item-group-${it.title}`
+          }
+        >
+          {it.children.map((child) =>
+            renderNavigationChildItem(child, it.title, sectionName),
+          )}
         </NavSubItemGroup>
       </NavCategory>
     ),
@@ -83,13 +118,13 @@ export const Navigator = (props: NavigatorProps) => {
 
   const renderNavigationGroup = useCallback(
     (gp: NavigationGroup) => (
-      <React.Fragment key={`nav-group-${gp.title}`}>
+      <React.Fragment key={`nav-section-${gp.title}`}>
         <NavSectionHeader>{gp.title}</NavSectionHeader>
         {gp.items.map((it) =>
           isNavigationItemWithChildren(it)
-            ? renderNavigationItemWithChildren(it)
+            ? renderNavigationItemWithChildren(it, gp.title)
             : isNavigationItem(it)
-              ? renderNavigationItem(it)
+              ? renderNavigationItem(it, gp.title)
               : null,
         )}
       </React.Fragment>
