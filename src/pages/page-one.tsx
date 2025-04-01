e2491cfd414d0079730086d7853dbd537dd52ed0
Chiky Chan <100360506+ChikyChan@users.noreply.github.com>
2025-04-01 23:58:15 +0800

    feat: add web api mock support (#2)
    * chore: rename app.tsx
    
    * feat: add web api mock support
    
    * chore: remove console.log

--- src/pages/page-one.tsx
+++ src/pages/page-one.tsx
@@ -1,5 +1,11 @@
+import { useEffect } from 'react'
 import { Text } from '@fluentui/react-components'
 
 export const PageOne = () => {
+  useEffect(() => {
+    fetch('/api/user/me')
+      .then((response) => response.json())
+      .then((data) => alert(JSON.stringify(data, null, 2)))
+  }, [])
   return <Text>Page One</Text>
 }
